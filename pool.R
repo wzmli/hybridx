3cdfecf4776dab80c8ae085af6db7f9ddd9bd7e8
Michael Li <wzmichael.li@gmail.com>
2017-05-15 14:22:02 -0400

    overwhelming

---
+++ pool.R
@@ -0,0 +1,19 @@
+targetname <- unlist(strsplit(rtargetname,"_"))
+
+print(targetname)
+print(targetname[2])
+
+
+dirpath <- "./jags_dir/results/"
+if(targetname[3] == "stan"){
+dirpath <- "./stan_dir/results/"}
+if(targetname[3] == "nim"){
+dirpath <- "./nimble_dir/results/"}
+
+print(dirpath)
+
+fn <- list.files(path=dirpath,pattern=targetname[2])
+ll <- lapply(fn, function(x)readRDS(paste(dirpath,x,sep=""))[[1]])
+cc <- do.call(c,ll)
+
+saveRDS(cc,file=paste(dirpath,targetname[2],targetname[3],".RDS",sep=""))
