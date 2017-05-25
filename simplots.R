248ce52fd84ee74f5f212b2c9ba460f76a44e9ab
wzmli <wzmichael.li@gmail.com>
2017-05-25 17:27:58 -0400

    manually change y axis labels

--- simplots.R
+++ simplots.R
@@ -4,7 +4,7 @@ library(gridExtra)
 
 load('.forecastplot_dis.1.bb.bb.3_jags.RData')
 
-g1 <- gg+ theme(legend.position = "none")
+g1 <- gg+ theme(legend.position = "none") + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
 
 load(".forecastplot_hyb.1.bb.nb.12_stan.RData")
 
@@ -14,10 +14,10 @@ g2 <- gg + theme(legend.position = "none")
 load('.forecastplot_dis.1.b.b.19_jags.RData')
 
 
-g3 <- gg + theme(legend.position = "none")
+g3 <- gg + theme(legend.position = "none") + scale_y_continuous(trans="log1p",breaks=c(1,500,1000,2000,6000))
 
 load(".forecastplot_dis.1.bb.b.19_nim.RData")
 
-g4 <- gg + theme(legend.position = "none")
+g4 <- gg + theme(legend.position = "none") + scale_y_continuous(trans="log1p",breaks=c(1,500,1000,2000,8000))
 
 print(grid.arrange(g1,g2,g3,g4,nrow=2))
