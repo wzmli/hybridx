08870bc54febeb40db3498cbb4ff4a08c08f4dfb
wzmli <wzmichael.li@gmail.com>
2017-05-26 10:17:01 -0400

    we can see the different level dispersion effects

--- simplots.R
+++ simplots.R
@@ -2,21 +2,21 @@ library(ggplot2)
 library(gridExtra)
 
 
-load('.forecastplot_dis.1.b.b.3_jags.RData')
+load('.forecastplot_dis.1.b.b.12_nim.RData')
 
-g1 <- gg+ ggtitle("dis.1.b.b.jags")+ theme(legend.position = "none") + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
+g1 <- gg+ ggtitle("dis.1.b.b.nim")+ theme(legend.position = "none") # + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
 
-load(".forecastplot_hyb.1.b.nb.3_stan.RData")
+load(".forecastplot_dis.1.b.bb.12_nim.RData")
 
-g2 <- gg+ ggtitle("hyb.1.b.nb.stan")+ theme(legend.position = "none") + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
+g2 <- gg+ ggtitle("dis.1.b.bb.jags")+ theme(legend.position = "none") # + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
 
-load('.forecastplot_hyb.1.bb.p.3_stan.RData')
+load('.forecastplot_dis.1.bb.b.12_nim.RData')
 
 
-g3 <-gg+ ggtitle("hyb.1.bb.p.stan")+ theme(legend.position = "none") + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
+g3 <-gg+ ggtitle("dis.1.bb.p.jags")+ theme(legend.position = "none") # + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
 
-load(".forecastplot_dis.1.bb.bb.3_nim.RData")
+load(".forecastplot_dis.1.bb.bb.12_nim.RData")
 
-g4 <- gg+ ggtitle("dis.1.bb.bb.nim")+ theme(legend.position = "none") + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
+g4 <- gg+ ggtitle("dis.1.bb.bb.nim")+ theme(legend.position = "none") # + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
 
 print(grid.arrange(g1,g2,g3,g4,nrow=2))
