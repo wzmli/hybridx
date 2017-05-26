ea5be5cbf8f5dc086a9133766426c8b3410ea04d
wzmli <wzmichael.li@gmail.com>
2017-05-26 12:16:55 -0400

    a thoughtful way to show different combinations

--- simplots.R
+++ simplots.R
@@ -6,17 +6,17 @@ load('.forecastplot_dis.1.b.b.12_nim.RData')
 
 g1 <- gg+ ggtitle("dis.1.b.b.nim")+ theme(legend.position = "none") # + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
 
-load(".forecastplot_dis.1.b.bb.12_nim.RData")
+load(".forecastplot_hyb.2.p.nb.12_stan.RData")
 
-g2 <- gg+ ggtitle("dis.1.b.bb.jags")+ theme(legend.position = "none") # + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
+g2 <- gg+ ggtitle("hyb.2.p.nb.stan")+ theme(legend.position = "none") # + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
 
-load('.forecastplot_dis.1.bb.b.12_nim.RData')
+load('.forecastplot_dis.2.bb.p.12_jags.RData')
 
 
-g3 <-gg+ ggtitle("dis.1.bb.b.jags")+ theme(legend.position = "none") # + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
+g3 <-gg+ ggtitle("dis.2.bb.p.jags")+ theme(legend.position = "none") # + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
 
-load(".forecastplot_dis.1.bb.bb.12_nim.RData")
+load(".forecastplot_dis.1.nb.bb.12_nim.RData")
 
-g4 <- gg+ ggtitle("dis.1.bb.bb.nim")+ theme(legend.position = "none") # + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
+g4 <- gg+ ggtitle("dis.1.nb.bb.nim")+ theme(legend.position = "none") # + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
 
 print(grid.arrange(g1,g2,g3,g4,nrow=2))
