625b0bcadc4689e6659b9f08b30fd3b79e40a6d7
wzmli <wzmichael.li@gmail.com>
2017-05-26 14:05:50 -0400

    i think I am going to stop here

--- simplots.R
+++ simplots.R
@@ -38,7 +38,7 @@ g6 <- gg+ ggtitle("dis.1.b.bb.nim")+ theme(legend.position = "none") # + scale_y
 
 load(".forecastplot_hyb.2.nb.p.138_stan.RData")
 
-g7 <- gg+ ggtitle("hyb.2.nb.p.nim")+ theme(legend.position = "none") # + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
+g7 <- gg+ ggtitle("hyb.2.nb.p.stan")+ theme(legend.position = "none") # + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
 
 
 load(".forecastplot_hyb.1.bb.nb.138_nim.RData")
@@ -46,3 +46,29 @@ load(".forecastplot_hyb.1.bb.nb.138_nim.RData")
 g8 <- gg+ ggtitle("hyb.1.bb.nb.nim")+ theme(legend.position = "none") # + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
 
 print(grid.arrange(g5,g6,g7,g8,nrow=2))
+
+
+
+## set 3
+
+load(".forecastplot_hyb.2.p.p.86_stan.RData")
+
+g5 <- gg+ ggtitle("hyb.2.p.p.stan")+ theme(legend.position = "none") # + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
+
+
+load(".forecastplot_dis.1.b.bb.86_jags.RData")
+
+g6 <- gg+ ggtitle("dis.1.b.bb.jags")+ theme(legend.position = "none") # + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
+
+
+load(".forecastplot_dis.1.bb.b.86_nim.RData")
+
+g7 <- gg+ ggtitle("dis.2.bb.b.nim")+ theme(legend.position = "none") # + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
+
+
+load(".forecastplot_hyb.2.nb.nb.86_jags.RData")
+
+g8 <- gg+ ggtitle("hyb.2.nb.nb.jags")+ theme(legend.position = "none") # + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
+
+print(grid.arrange(g5,g6,g7,g8,nrow=2))
+
