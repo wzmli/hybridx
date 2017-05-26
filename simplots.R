bd2c443ae03393225db8f5480801e95736b17755
mli2830 <wzmichael.li@gmail.com>
2017-05-25 22:53:32 -0400

    a more instructive way to display the plots

--- simplots.R
+++ simplots.R
@@ -2,22 +2,21 @@ library(ggplot2)
 library(gridExtra)
 
 
-load('.forecastplot_dis.1.bb.bb.3_jags.RData')
+load('.forecastplot_dis.1.b.b.3_jags.RData')
 
-g1 <- gg+ theme(legend.position = "none") + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
+g1 <- gg+ ggtitle("dis.1.b.b.jags")+ theme(legend.position = "none") + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
 
-load(".forecastplot_hyb.1.bb.nb.12_stan.RData")
+load(".forecastplot_hyb.1.b.nb.3_stan.RData")
+
+g2 <- gg+ ggtitle("hyb.1.b.nb.stan")+ theme(legend.position = "none") + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
+
+load('.forecastplot_hyb.1.bb.p.3_stan.RData')
 
 
-g2 <- gg + theme(legend.position = "none")
+g3 <-gg+ ggtitle("hyb.1.bb.p.stan")+ theme(legend.position = "none") + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
 
-load('.forecastplot_dis.1.b.b.19_jags.RData')
+load(".forecastplot_dis.1.bb.bb.3_nim.RData")
 
-
-g3 <- gg + theme(legend.position = "none") + scale_y_continuous(trans="log1p",breaks=c(1,500,1000,2000,6000))
-
-load(".forecastplot_dis.1.bb.b.19_nim.RData")
-
-g4 <- gg + theme(legend.position = "none") + scale_y_continuous(trans="log1p",breaks=c(1,500,1000,2000,8000))
+g4 <- gg+ ggtitle("dis.1.bb.bb.nim")+ theme(legend.position = "none") + scale_y_continuous(trans="log1p",breaks=c(0,500,1000,2000,6000))
 
 print(grid.arrange(g1,g2,g3,g4,nrow=2))
